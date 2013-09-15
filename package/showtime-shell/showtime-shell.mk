4d4d471da1d08f2ad37b696f2d2f4d6d8861bdf3
Andreas Öman <andreas@lonelycoder.com>
2013-09-15 08:51:06 +0200

    showtime-shell: Add package

---
+++ package/showtime-shell/showtime-shell.mk
@@ -0,0 +1,19 @@
+SHOWTIME_SHELL_VERSION = 15b081870a909ef11c61483e4fdbb74d079ebf0f
+SHOWTIME_SHELL_SITE = git://github.com/andoma/showtime-shell.git
+SHOWTIME_SHELL_LICENSE = GPLv3
+
+define SHOWTIME_SHELL_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 package/showtime-shell/S60showtime-shell \
+		$(TARGET_DIR)/etc/init.d/S60showtime-shell
+endef
+
+
+define SHOWTIME_SHELL_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/showtime-shell $(TARGET_DIR)/usr/sbin/showtime-shell
+endef
+
+define SHOWTIME_SHELL_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" -C $(@D)
+endef
+
+$(eval $(generic-package))
